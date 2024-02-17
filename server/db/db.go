cc02797e4893cf60b082e2288e121a53ec6b7b21
Vinh Ky Nguyen <info.vinhky@gmail.com>
2024-02-17 16:01:50 +0700

    Init makefile and database

---
+++ server/db/db.go
@@ -0,0 +1,24 @@
+package db
+
+import "database/sql"
+
+type Database struct {
+	db *sql.DB
+}
+
+func NewDatabase() (*Database, error) {
+	db, err := sql.Open("postgres", "postgresql://root:root@localhost:5433/go-chat?sslmode=disable")
+	if err != nil {
+		return nil, err
+	}
+
+	return &Database{db: db}, nil
+}
+
+func (d *Database) Close() {
+	d.db.Close()
+}
+
+func (d *Database) GetBD() *sql.DB {
+	return d.db
+}
