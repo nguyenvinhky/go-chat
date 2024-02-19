943da24e84813937932c9594a722daab7b16cbb7
Vinh Ky Nguyen <info.vinhky@gmail.com>
2024-02-19 22:34:30 +0700

    config create api add user

---
+++ server/internal/user/user.go
@@ -0,0 +1,40 @@
+package user
+
+import "context"
+
+type User struct {
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CreateUserReq struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CreateUserRes struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+type LoginUserReq struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginUserRes struct {
+	accessToken string
+	ID          string `json:"id"`
+	Username    string `json:"username"`
+}
+type Repository interface {
+	CreateUser(ctx context.Context, user *User) (*User, error)
+}
+
+type Service interface {
+	CreateUser(ctx context.Context, req *CreateUserReq) (*CreateUserRes, error)
+}
